e0107a3f890aa0d1710bb3fe87eadc862926e482
Sorrien <collin.sparks@gmail.com>
2018-03-31 15:00:58 -0400

    Add project files.

---
+++ RoleManagerBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RoleManagerBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
